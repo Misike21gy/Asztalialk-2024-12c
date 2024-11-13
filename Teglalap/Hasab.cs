0a4b2434bc41384a1e46fb9205122c593ff6382c
Misike21gy <2021129@dszcberegszaszi.hu>
2024-11-13 09:49:02 +0100

    feladatok+

---
+++ Teglalap/Hasab.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teglalap
+{
+    class Hasab : teglalap
+    {
+        protected double Talap = 3,
+           Tpalast = 6,
+           magassag = 12,
+           felszin = 0;
+        public szamitas(double a, double b, double c)
+        {
+            this.Talap = a;
+            this.Tpalast = b;
+            this.magassag = c;
+            SetFelszin();
+            this.felszin = this.Talap * 2 + this.Tpalast;
+        }
+        public double GetTerulet() { return this.GetTerulet(); }
+    }
+}
---
+++ Teglalap/teglalap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teglalap
+{
+    class teglalap
+    {
+        protected double teruleta = 8,
+            teruletb = 4,
+            terulet = 0;
+
+        public szamitas(double a, double b)
+        {
+            this.teruleta = a;
+            this.teruleta = b;
+            SetTerulet();
+            this.terulet = this.teruleta * this.teruletb;
+        }
+        public double GetTerulet() { return this.GetTerulet(); }
+    }
+}
