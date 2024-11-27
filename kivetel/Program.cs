a77ff85fa93344716d3df1d318b31f4e9bf7693d
Misike21gy <2021129@dszcberegszaszi.hu>
2024-11-27 10:06:14 +0100

    mentés

---
+++ kivetel/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kivetel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Osztály példányosítása
+            kivetelosztaly k = new kivetelosztaly();
+
+            //k.KivKezNincs()
+            //k.KivKezAlt()
+            //k.kivKezOs()
+
+        }
+    }
+}
---
+++ kivetel/kivetelosztaly.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kivetel
+{
+    class kivetelosztaly
+    {
+        public void KivKezNincs()
+        {
+            Console.WriteLine("Nincs kivételkezeles\n Adjon meg egy értéket!");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("A beírt szám{0}", szam);
+
+        }
+
+        public void KivKezAlt()
+        {
+            Console.WriteLine("\nHiba objektum nélküli kivételkezelés\nAdjon meg egy karaktert!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A beírt szám: szam}", szam);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void KivKezOs()
+        {
+            Console.WriteLine("\nŐs Hiba objektummal kivételkezelés\n Adjon meg egy karaktert!");
+            try
+            {
+                int szam = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("A beírt szám: {0}", szam);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine();
+            }
+        }
+    Console.ReadKey();
+    }
+}
