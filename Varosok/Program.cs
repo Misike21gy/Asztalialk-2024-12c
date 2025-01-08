deeca72161cca1c8a61b0cc15c72c6ae3b5cce56
Misike21gy <2021129@dszcberegszaszi.hu>
2025-01-08 10:06:07 +0100

    mentes

---
+++ Varosok/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varosok
+{
+    class Program
+    {
+        static void setDB()
+        {
+            int nevszam;
+            Console.WriteLine("Hány nevet akarsz beolvasni?");
+            nevszam = Console.ReadLine();
+        }
+
+        static void SetNevek()
+        {
+            string nevek;
+            Console.WriteLine("Add meg a neveket!");
+            nevek = Console.ReadLine();
+
+        }
+
+        static void getNevek()
+        {
+
+        }
+
+        static void getVan()
+        {
+            string van;
+            Console.WriteLine("Adj meg egy nevet");
+            van = Console.ReadLine();
+
+        }
+
+        static void delNev()
+        {
+
+        }
+
+        static void delLAst()
+        {
+
+        }
+
+    }
+}
---
+++ Varosok/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Varosok")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Varosok")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e7b218d-619e-4533-8138-8d3aad3e6238")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
