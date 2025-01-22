18db9489a7e30f8885e832871c6c4ab6a92f8642
Misike21gy <2021129@dszcberegszaszi.hu>
2025-01-22 11:33:36 +0100

    save

--- szofteszt01.15/Program.cs
+++ szofteszt01.15/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace szofteszt01._15
+namespace ConsoleApp8
 {
     class Program
     {
@@ -18,42 +18,64 @@ namespace szofteszt01._15
         }
         static void Main(string[] args)
         {
-            static void Main(string[] args)
-        {
-                bool vanfile = File.Exists("nevek.txt");
-                if (vanfile)
-                {
 
-                }
-                   string[] kategoriak = File.ReadAllLines("nevek.txt");
-                                   int i;
-                
+            bool vanfile = File.Exists("nevek.txt");
+            if (vanfile)
+            {
+                string[] kategoriak = File.ReadAllLines("nevek.txt");
+
+
+
+                int i;
 
                 for (i = 0; i < kategoriak.Length; i++)
                 {
                     Console.WriteLine(kategoriak[i]);
                 }
-
                 Adat[] adatok = new Adat[kategoriak.Length];
 
-                for (i = 0; i < kategoriak.Length; i++)
+                for (i = 1; i < kategoriak.Length; i++)
                 {
                     string[] sorok = kategoriak[i].Split(' ');
                     adatok[i].nev = sorok[0];
-                    adatok[i].oraber = int.Parse(sorok[1]);
+                    try
+                    {
+                        adatok[i].oraber = int.Parse(sorok[1]);
+                    }
+                    catch (Exception e)
+                    {
+                        adatok[i].oraber = 0;
+                    }
+
                     adatok[i].kolis = sorok[2];
                     adatok[i].lakhely = sorok[3];
                 }
 
                 Console.WriteLine("Az adatok tömb elemei:");
 
-                for (i = 0; i < kategoriak.Length; i++)
+                for (i = 1; i < kategoriak.Length; i++)
                 {
-                    Console.WriteLine("{0}   {1}   {2}  {3}", adatok[i].nev, adatok[i].oraber, adatok[i].kolis, adatok[i].lakhely;
+                    Console.WriteLine("{0}   {1}   {2}  {3}  ", adatok[i].nev, adatok[i].oraber, adatok[i].kolis, adatok[i].lakhely);
                 }
 
-                else
-                Console.ReadKey();
+                string[] vissza = new string[kategoriak.Length];
+                for (i = 0; i < kategoriak.Length; i++)
+                {
+                    vissza[i] = adatok[i].nev + " " + adatok[i].oraber + " " + adatok[i].kolis + " " + adatok[i].lakhely;
+                }
+
+                Console.WriteLine("Az uj string tömb:");
+                for (i = 0; i < kategoriak.Length; i++)
+                {
+                    Console.WriteLine(vissza[i]);
+                }
+                File.WriteAllLines("vissza.txt", vissza);
+            }
+            else
+                Console.WriteLine("nincs ilyen file");
+
+
+            Console.ReadKey();
         }
     }
 }
