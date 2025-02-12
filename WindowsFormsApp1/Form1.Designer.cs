e04b9fdd13b43528de5404ba1574e99ef89b0916
Misike21gy <2021129@dszcberegszaszi.hu>
2025-02-12 10:09:18 +0100

    mentes

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKattints = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKattints
+            // 
+            this.btnKattints.Location = new System.Drawing.Point(298, 185);
+            this.btnKattints.Name = "btnKattints";
+            this.btnKattints.Size = new System.Drawing.Size(148, 68);
+            this.btnKattints.TabIndex = 0;
+            this.btnKattints.Text = "Kattints rám!";
+            this.btnKattints.UseVisualStyleBackColor = true;
+            this.btnKattints.Click += new System.EventHandler(this.btnKattints_Click);
+            this.btnKattints.MouseLeave += new System.EventHandler(this.btnKattints_MouseLeave);
+            this.btnKattints.MouseHover += new System.EventHandler(this.btnKattints_MouseHover);
+            // 
+            // MainForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKattints);
+            this.Name = "MainForm1";
+            this.Text = "Próbálgatás";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKattints;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm1 : Form
+    {
+        //Osztályváltozók
+        private bool iranyFel = true,
+            iranyBalra = true;
+        private int lepesFel = 20,
+            lepesOldalt = 30;
+
+        public MainForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKattints_Click(object sender, EventArgs e)
+        {
+            Point point = btnKattints.Location;
+            //Megvizsgáljuk hogy nem megyek-e ki a formból
+            if (iranyFel && (point.Y - lepesFel) < 0)
+            {
+                //Függőleges irányt váltunk
+                iranyFel = false;
+            }
+            //Vízszintes irányt váltunk
+            if (iranyFel && (point.Y - lepesFel) < 0)
+            {
+                //Vízszintes irányt váltunk
+                iranyBalra = false;
+            }
+
+            //Megvizsgáljuk hogy nem megyek-e ki a formból lefele és jobbra
+            if (!iranyFel && (point.Y - btnKattints.Height) + lepesFel < Height)
+            {
+                //Függőleges irányt váltunk
+                iranyFel = true;
+            }
+            //Vízszintes irányt váltunk
+            if (!iranyBalra && (point.X - btnKattints.Width)-lepesOldalt < 0)
+            {
+                //Vízszintes irányt váltunk
+                iranyBalra = true;
+            }
+            //Beállítom a gomb pozícióját
+            point.X += iranyBalra ? lepesOldalt: -lepesOldalt;
+            point.Y += iranyFel ? lepesFel : lepesFel;
+            btnKattints.Location = point;
+
+        }
+
+        private void btnKattints_MouseHover(object sender, EventArgs e)
+        {
+            btnKattints.BackColor = Color.Orange;
+        }
+
+        private void btnKattints_MouseLeave(object sender, EventArgs e)
+        {
+            btnKattints.BackColor = Color.LightBlue;
+            btnKattints.Text = "Kattints rám!";
+        }
+
+       
+    }
+}
