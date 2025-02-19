a227e648e8a08cc3d759d3befb4468a7d1711dfd
Misike21gy <2021129@dszcberegszaszi.hu>
2025-02-19 08:47:55 +0100

    mentés

---
+++ Kurzor-Form/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kurzor_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Kimentem az egér új pozícióját
+            int n = e.X;
+            int m=e.Y;
+
+            //A pocíziciókat kiírom a labelre
+            lblSzoveg.Text=($"X:{n} Y:{m}");
+            lblSzoveg.Top = m;
+            lblSzoveg.Left = n;
+        }
+    }
+}
