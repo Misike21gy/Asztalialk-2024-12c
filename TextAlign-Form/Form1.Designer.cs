2b18a2275997fe4656d96a29e0e0f076ed23a8a4
Misike21gy <2021129@dszcberegszaszi.hu>
2025-02-19 08:34:59 +0100

    Label text align

---
+++ TextAlign-Form/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TextAlign_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(734, 414);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kattints valahova!";
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 414);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Label Text Align";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.l);
+            this.ResumeLayout(false);
+
+        }
+
+        private void l(object sender, MouseEventArgs e)
+        {
+            Random rand = new Random();
+            int x = rand.Next(0, this.ClientSize.Width - label1.Width);
+            int y = rand.Next(0, this.ClientSize.Height - label1.Height);
+            label1.Location = new System.Drawing.Point(x, y);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
