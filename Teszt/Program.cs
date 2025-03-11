b76e73b640a0b522bb7f9d7c3a87e020a79db2c3
Mihály Máté Gyönyörű <2021129@dszcberegszaszi.hu>
2025-03-11 23:16:04 +0100

    mentes

--- Teszt/Program.cs
+++ Teszt/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Teszt
 {
@@ -10,6 +11,24 @@ namespace Teszt
     {
         static void Main(string[] args)
         {
+            String[] teszt= File.ReadAllLines("macska.txt");
+            if (File.Exists("macska.txt" ) ==true)
+            {
+                Console.WriteLine("A fájl létezik");
+            }
+            else
+            {
+                Console.WriteLine("A fájl nem létezik");
+            }
+            Console.WriteLine(teszt);
+            if (teszt[3][0] == 'f')
+            {
+                Console.WriteLine("van");
+            }
+            else
+            {
+                Console.WriteLine("nincs");
+            }
             Console.ReadKey();
         }
     }
---
+++ szofteszt-gyakorlas/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("szofteszt-gyakorlas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("szofteszt-gyakorlas")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0e68e34e-7da9-4194-8512-866f40a34165")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
