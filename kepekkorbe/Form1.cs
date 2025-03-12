105583569ebf9fb52783d8e09f25e44e2215180b
Misike21gy <2021129@dszcberegszaszi.hu>
2025-03-12 08:20:38 +0100

    mentes0312

--- kepekkorbe/Form1.cs
+++ kepekkorbe/Form1.cs
@@ -17,17 +17,32 @@ namespace kepekkorbe
         {
             InitializeComponent();
         }
-
         
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void label_jobbra_Click(object sender, EventArgs e)
         {
-            PictureBox3 = change.Image(Resources / fej1.jpg);
+            Image elso = pictureBox1.Image;
+            pictureBox1.Image = pictureBox2.Image;
+            pictureBox2.Image = pictureBox3.Image;
+            pictureBox3.Image = pictureBox4.Image;
+            pictureBox4.Image = pictureBox5.Image;
+            pictureBox5.Image = pictureBox6.Image;
+            pictureBox6.Image = pictureBox7.Image;
+            pictureBox7.Image = pictureBox8.Image;
+            pictureBox8.Image = elso;
+        }
+
+        private void label_balra_Click(object sender, EventArgs e)
+        {
+            Image elso = pictureBox1.Image;
+            pictureBox1.Image = pictureBox8.Image;
+            pictureBox8.Image = pictureBox7.Image;
+            pictureBox7.Image = pictureBox6.Image;
+            pictureBox6.Image = pictureBox5.Image;
+            pictureBox5.Image = pictureBox4.Image;
+            pictureBox4.Image = pictureBox3.Image;
+            pictureBox3.Image = pictureBox2.Image;
+            pictureBox2.Image = elso;
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
@@ -35,7 +50,11 @@ namespace kepekkorbe
 
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
+        }
 
+        
     }
 }
