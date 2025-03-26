69bb2f0b063b27a507b2dbfba2deed65844963a9
Misike21gy <2021129@dszcberegszaszi.hu>
2025-03-26 09:26:59 +0100

    mentes

---
+++ mysql.users/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace mysql.users
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxID = new System.Windows.Forms.TextBox();
+            this.tbxnev = new System.Windows.Forms.TextBox();
+            this.tbxjelszo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.cbxAdmin = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tbxID
+            // 
+            this.tbxID.Location = new System.Drawing.Point(54, 36);
+            this.tbxID.Name = "tbxID";
+            this.tbxID.Size = new System.Drawing.Size(100, 20);
+            this.tbxID.TabIndex = 0;
+            // 
+            // tbxnev
+            // 
+            this.tbxnev.Location = new System.Drawing.Point(54, 100);
+            this.tbxnev.Name = "tbxnev";
+            this.tbxnev.Size = new System.Drawing.Size(100, 20);
+            this.tbxnev.TabIndex = 1;
+            // 
+            // tbxjelszo
+            // 
+            this.tbxjelszo.Location = new System.Drawing.Point(54, 166);
+            this.tbxjelszo.Name = "tbxjelszo";
+            this.tbxjelszo.Size = new System.Drawing.Size(100, 20);
+            this.tbxjelszo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Azonosító:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Név:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Jelszó:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Kapcsolódás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(317, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Megnyitás olvasásra";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(332, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Olvas";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(332, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Beszúrás";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(317, 235);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Módosítás kezdés";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(332, 297);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Törlés";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(54, 297);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(107, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Kapcsolat bontása";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // cbxAdmin
+            // 
+            this.cbxAdmin.AutoSize = true;
+            this.cbxAdmin.Location = new System.Drawing.Point(70, 235);
+            this.cbxAdmin.Name = "cbxAdmin";
+            this.cbxAdmin.Size = new System.Drawing.Size(61, 17);
+            this.cbxAdmin.TabIndex = 13;
+            this.cbxAdmin.Text = "Admin?";
+            this.cbxAdmin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 406);
+            this.Controls.Add(this.cbxAdmin);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxjelszo);
+            this.Controls.Add(this.tbxnev);
+            this.Controls.Add(this.tbxID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxID;
+        private System.Windows.Forms.TextBox tbxnev;
+        private System.Windows.Forms.TextBox tbxjelszo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.CheckBox cbxAdmin;
+    }
+}
+
---
+++ mysql.users/Form1.cs
@@ -0,0 +1,62 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mysql.users
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection mysqlConn;
+        private MySqlDataReader mysqlDr;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MysqlConnect;
+        }
+
+        private void mysqlConnect()
+        {
+            MySqlConnectionStringBuilder sb= new MySqlConnectionStringBuilder();
+            sb.Server= "localhost";
+            sb.UserID= "root";
+            sb.Password= "";
+            sb.Database = "iktat";
+
+            try
+            {
+                mysqlConn = new MySqlConnection(sb.ToString());
+                mysqlConn.Open();
+                MessageBox.Show(openSikeres);
+
+                FormState = FormState.Opened;
+                buttonSwitch(formState);
+            }
+            catch(Exception ex)
+            {
+                MessageBox
+            }
+
+
+        }
+    }
+}
