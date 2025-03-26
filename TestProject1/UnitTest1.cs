df9c828543451f298a7308d56fac28ab18bbfbd4
Misike21gy <2021129@dszcberegszaszi.hu>
2025-03-26 11:58:40 +0100

    mentés

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace auto
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Car auto1 = new Car();
+        }
+    }
+}
---
+++ auto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Car auto1 = new Car();
+            Console.WriteLine(auto1.color);
+            auto1.color = "blue";
+            Console.WriteLine(auto1.color);
+        }
+    }
+}
